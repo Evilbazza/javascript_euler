2882a9b7c54d3fb31aca1d1352885a3950fec0c8
Evilbazza <joe.barratt@gmail.com>
2014-04-15 21:59:51 +0100

    now working

--- problem_3.js
+++ problem_3.js
@@ -1,22 +1,24 @@
-
-var isPrime = function(number) {
-  for (i=2; i<number; i++) {
-      if (number % i === 0) {
-          return false;
-      }
-  }
-  return true;
+var isPrime = function(number) { 
+    if (number % 2 === 0){
+        return false;
+    }
+    var max = Math.sqrt(number);
+    for (i=3; i<=max; i+=2) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+ return true;
 };
 
-var num = 13195;
+var num = 600851475143;
 var factors = [];
 
-for (i=2; i<num; i++) {
+for (i=2; i<=Math.floor(Math.sqrt(num)); i++) {
     if (num % i === 0) {
         factors.push(i);
     }
 }
-
 var primeFactors = [];
 
 for (j=0; j<factors.length; j++) {
@@ -24,4 +26,5 @@ for (j=0; j<factors.length; j++) {
         primeFactors.push(factors[j]);
     }
 }
-console.log(primeFactors);
+var largest = Math.max.apply(0, primeFactors);
+console.log(largest);
